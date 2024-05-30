7ee326162ece6bbcfea4ad547632fd75435b8e77
Jiarui <1457866277@qq.com>
2024-05-30 22:19:15 +0800

    d

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+// 城市坐标
 var geoCoordMap = {
   上海: [121.4648, 31.2891],
   东莞: [113.8953, 22.901],
@@ -119,12 +120,12 @@ var color = ["#a6c84c", "#ffa022", "#46bee9"]; //航线的颜色
 var planePath =
   "path://M1705.06,1318.313v-89.254l-319.9-221.799l0.073-208.063c0.521-84.662-26.629-121.796-63.961-121.491c-37.332-0.305-64.482,36.829-63.961,121.491l0.073,208.063l-319.9,221.799v89.254l330.343-157.288l12.238,241.308l-134.449,92.931l0.531,42.034l175.125-42.917l175.125,42.917l0.531-42.034l-134.449-92.931l12.238-241.308L1705.06,1318.313z";
 //var planePath = 'arrow';
+// 航线类别的颜色
 var classColorMap = {
   "普通航线": "#ffffff", 
   "断路航线": "#FF0000", 
   "绿色通道": "#c0ff60"  
 };
-
 
 //页面上面的数字
 (function () {
@@ -153,8 +154,7 @@ var classColorMap = {
 })();
 
 
-// 点击城市
-// 修改后的图表点击事件处理器
+// 点击城市查看城市详情
 myChart.on('click', function (params) {
   if (params.seriesType === 'effectScatter') {
     const city = params.name;
@@ -167,7 +167,38 @@ myChart.on('click', function (params) {
   }
 });
 
-//特快专递
+
+// 获取全图
+(function () {
+  // 页面加载完成后发送 GET 请求
+  document.addEventListener('DOMContentLoaded', function() {
+    fetch('https://run.mocky.io/v3/c9304522-8e77-43ff-97bb-32844bab1bf3', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('网络响应错误');
+      }
+      return response.json();
+    })
+    .then(json => {
+      var routes = json.data;
+      var cityColorValue = "#ffa060";
+      var lineColorValue = "#ffa060";
+      // 更新地图
+      updateMapWithCitiesAndRoutes(routes, cityColorValue, lineColorValue);
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+  });
+})();
+
+
+// 特快专递
 (function () {
   var fromInput = document.getElementById('fromInput');
   var toInput = document.getElementById('toInput');
@@ -218,7 +249,7 @@ myChart.on('click', function (params) {
   }
 })();
 
-//物资补给
+// 物资补给
 (function () {
   var goodsCityInput = document.getElementById('goodsCityInput');
   var goodsTypeInput = document.getElementById('goodsTypeInput');
@@ -262,8 +293,7 @@ myChart.on('click', function (params) {
 })();
 
 
-
-// 灾害上报功能
+// 灾害上报
 (function () {
   var disasterFromInput = document.getElementById('fromInput');
   var disasterToInput = document.getElementById('toInput');
@@ -345,7 +375,7 @@ myChart.on('click', function (params) {
   });
 })();
 
-
+// 模式切换
 document.addEventListener('DOMContentLoaded', function () {
   var modeInputs = document.querySelectorAll('input[name="mode"]');
   var features = {
@@ -438,8 +468,6 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 });
 
-
-
 //刷新地图的函数
 function updateMapWithCitiesAndRoutes(routes, cityColorValue, lineColorValue) {
   // 准备城市散点数据
@@ -448,7 +476,7 @@ function updateMapWithCitiesAndRoutes(routes, cityColorValue, lineColorValue) {
     citiesSet.add(route.from);
     citiesSet.add(route.to);
   });
-
+  // 准备城市散点数据
   var scatterData = Array.from(citiesSet).map(city => {
     var geoCoord = geoCoordMap[city];
     if (geoCoord) {
@@ -483,6 +511,7 @@ function updateMapWithCitiesAndRoutes(routes, cityColorValue, lineColorValue) {
     return null;
   }).filter(item => item !== null);
 
+  // 准备拖尾的数据
   var tailsData = routes.map(route => {
     var fromCoord = geoCoordMap[route.from];
     var toCoord = geoCoordMap[route.to];
@@ -490,7 +519,7 @@ function updateMapWithCitiesAndRoutes(routes, cityColorValue, lineColorValue) {
       return {
         fromName: route.from,
         toName: route.to,
-        coords: [fromCoord, toCoord]
+        coords: [fromCoord, toCoord],
       };
     }
     return null;
@@ -638,203 +667,203 @@ function updateMapWithCitiesAndRoutes(routes, cityColorValue, lineColorValue) {
 
 }
 
-// 模拟飞行路线模块地图模块
-(function () {
-  //var myChart = echarts.init(document.querySelector(".map .chart"));
+// // 模拟飞行路线模块地图模块
+// (function () {
+//   //var myChart = echarts.init(document.querySelector(".map .chart"));
 
-  var XAData = [
-    [{ name: "西安" }, { name: "拉萨", value: 100 }],
-    [{ name: "西安" }, { name: "上海", value: 100 }],
-    [{ name: "西安" }, { name: "广州", value: 100 }],
-    [{ name: "西安" }, { name: "西宁", value: 100 }],
-    [{ name: "西安" }, { name: "银川", value: 100 }]
-  ];
+//   var XAData = [
+//     [{ name: "西安" }, { name: "拉萨", value: 100 }],
+//     [{ name: "西安" }, { name: "上海", value: 100 }],
+//     [{ name: "西安" }, { name: "广州", value: 100 }],
+//     [{ name: "西安" }, { name: "西宁", value: 100 }],
+//     [{ name: "西安" }, { name: "银川", value: 100 }]
+//   ];
 
-  var XNData = [
-    [{ name: "西宁" }, { name: "北京", value: 100 }],
-    [{ name: "西宁" }, { name: "上海", value: 100 }],
-    [{ name: "西宁" }, { name: "广州", value: 100 }],
-    [{ name: "西宁" }, { name: "西安", value: 100 }],
-    [{ name: "西宁" }, { name: "银川", value: 100 }]
-  ];
+//   var XNData = [
+//     [{ name: "西宁" }, { name: "北京", value: 100 }],
+//     [{ name: "西宁" }, { name: "上海", value: 100 }],
+//     [{ name: "西宁" }, { name: "广州", value: 100 }],
+//     [{ name: "西宁" }, { name: "西安", value: 100 }],
+//     [{ name: "西宁" }, { name: "银川", value: 100 }]
+//   ];
 
-  var YCData = [
-    [{ name: "拉萨" }, { name: "潍坊", value: 100 }],
-    [{ name: "拉萨" }, { name: "哈尔滨", value: 100 }],
-    [{ name: "银川" }, { name: "上海", value: 100 }],
-    [{ name: "银川" }, { name: "西安", value: 100 }],
-    [{ name: "银川" }, { name: "西宁", value: 100 }],
-    [{ name: "银川" }, { name: "青岛", value: 100 }]
-  ];
+//   var YCData = [
+//     [{ name: "拉萨" }, { name: "潍坊", value: 100 }],
+//     [{ name: "拉萨" }, { name: "哈尔滨", value: 100 }],
+//     [{ name: "银川" }, { name: "上海", value: 100 }],
+//     [{ name: "银川" }, { name: "西安", value: 100 }],
+//     [{ name: "银川" }, { name: "西宁", value: 100 }],
+//     [{ name: "银川" }, { name: "青岛", value: 100 }]
+//   ];
 
-  var planePath =
-    "path://M1705.06,1318.313v-89.254l-319.9-221.799l0.073-208.063c0.521-84.662-26.629-121.796-63.961-121.491c-37.332-0.305-64.482,36.829-63.961,121.491l0.073,208.063l-319.9,221.799v89.254l330.343-157.288l12.238,241.308l-134.449,92.931l0.531,42.034l175.125-42.917l175.125,42.917l0.531-42.034l-134.449-92.931l12.238-241.308L1705.06,1318.313z";
-  //var planePath = 'arrow';
-  var convertData = function (data) {
-    var res = [];
-    for (var i = 0; i < data.length; i++) {
-      var dataItem = data[i];
+//   var planePath =
+//     "path://M1705.06,1318.313v-89.254l-319.9-221.799l0.073-208.063c0.521-84.662-26.629-121.796-63.961-121.491c-37.332-0.305-64.482,36.829-63.961,121.491l0.073,208.063l-319.9,221.799v89.254l330.343-157.288l12.238,241.308l-134.449,92.931l0.531,42.034l175.125-42.917l175.125,42.917l0.531-42.034l-134.449-92.931l12.238-241.308L1705.06,1318.313z";
+//   //var planePath = 'arrow';
+//   var convertData = function (data) {
+//     var res = [];
+//     for (var i = 0; i < data.length; i++) {
+//       var dataItem = data[i];
 
-      var fromCoord = geoCoordMap[dataItem[0].name];
-      var toCoord = geoCoordMap[dataItem[1].name];
-      if (fromCoord && toCoord) {
-        res.push({
-          fromName: dataItem[0].name,
-          toName: dataItem[1].name,
-          coords: [fromCoord, toCoord],
-          value: dataItem[1].value
-        });
-      }
-    }
-    return res;
-  };
+//       var fromCoord = geoCoordMap[dataItem[0].name];
+//       var toCoord = geoCoordMap[dataItem[1].name];
+//       if (fromCoord && toCoord) {
+//         res.push({
+//           fromName: dataItem[0].name,
+//           toName: dataItem[1].name,
+//           coords: [fromCoord, toCoord],
+//           value: dataItem[1].value
+//         });
+//       }
+//     }
+//     return res;
+//   };
 
-  var color = ["#a6c84c", "#ffa022", "#46bee9"]; //航线的颜色
-  var series = [];
-  [
-    ["西安", XAData],
-    ["西宁", XNData],
-    ["银川", YCData]
-  ].forEach(function (item, i) {
-    series.push(
-      {
-        //name: item[0] + " Top3",
-        name: "temp",
-        type: "lines",
-        zlevel: 1,
-        effect: {
-          show: true,
-          period: 6,
-          trailLength: 0.7,
-          color: "red", //arrow箭头的颜色
-          symbolSize: 3
-        },
-        lineStyle: {
-          normal: {
-            color: color[i],
-            width: 0,
-            curveness: 0.2
-          }
-        },
-        data: convertData(item[1])
-      },
-      {
-        //name: item[0] + " Top3",
-        name: "temp",
-        type: "lines",
-        zlevel: 2,
-        symbol: ["none", "arrow"],
-        symbolSize: 10,
-        effect: {
-          show: true,
-          period: 6,
-          trailLength: 0,
-          symbol: planePath,
-          symbolSize: 15
-        },
-        lineStyle: {
-          normal: {
-            color: color[i],
-            width: 1,
-            opacity: 0.6,
-            curveness: 0.2
-          }
-        },
-        data: convertData(item[1])
-      },
-      {
-        name: "temp",
-        //name: "Cities",
-        type: "effectScatter",
-        coordinateSystem: "geo",
-        zlevel: 2,
-        rippleEffect: {
-          brushType: "stroke"
-        },
-        label: {
-          normal: {
-            show: true,
-            position: "right",
-            formatter: "{b}"
-          }
-        },
-        symbolSize: function (val) {
-          return val[2] / 8;
-        },
-        itemStyle: {
-          normal: {
-            color: color[i]
-          },
-          emphasis: {
-            areaColor: "#2B91B7"
-          }
-        },
-        data: item[1].map(function (dataItem) {
-          return {
-            name: dataItem[1].name,
-            value: geoCoordMap[dataItem[1].name].concat([dataItem[1].value])
-          };
-        })
-      }
-    );
-  });
-  var option = {
-    tooltip: {
-      trigger: "item",
-      formatter: function (params, ticket, callback) {
-        if (params.seriesType == "effectScatter") {
-          return "线路：" + params.data.name + "" + params.data.value[2];
-        } else if (params.seriesType == "lines") {
-          return (
-            params.data.fromName +
-            ">" +
-            params.data.toName +
-            "<br />" +
-            params.data.value
-          );
-        } else {
-          return params.name;
-        }
-      }
-    },
-    // legend: {
-    //   orient: "vertical",
-    //   top: "bottom",
-    //   left: "right",
-    //   data: ["西安 Top3", "西宁 Top3", "银川 Top3"],
-    //   textStyle: {
-    //     color: "#fff"
-    //   },
-    //   selectedMode: "multiple"
-    // },
-    geo: {
-      map: "china",
-      label: {
-        emphasis: {
-          show: true,
-          color: "#fff"
-        }
-      },
-      // 把中国地图放大了1.2倍
-      zoom: 1.2,
-      roam: true,
-      itemStyle: {
-        normal: {
-          // 地图省份的背景颜色
-          areaColor: "rgba(20, 41, 87,0.6)",
-          borderColor: "#195BB9",
-          borderWidth: 1
-        },
-        emphasis: {
-          areaColor: "#2B91B7"
-        }
-      }
-    },
-    series: series
-  };
-  myChart.setOption(option);
-  // 监听浏览器缩放，图表对象调用缩放resize函数
-  window.addEventListener("resize", function () {
-    myChart.resize();
-  });
-})();
+//   var color = ["#a6c84c", "#ffa022", "#46bee9"]; //航线的颜色
+//   var series = [];
+//   [
+//     ["西安", XAData],
+//     ["西宁", XNData],
+//     ["银川", YCData]
+//   ].forEach(function (item, i) {
+//     series.push(
+//       {
+//         //name: item[0] + " Top3",
+//         name: "temp",
+//         type: "lines",
+//         zlevel: 1,
+//         effect: {
+//           show: true,
+//           period: 6,
+//           trailLength: 0.7,
+//           color: "red", //arrow箭头的颜色
+//           symbolSize: 3
+//         },
+//         lineStyle: {
+//           normal: {
+//             color: color[i],
+//             width: 0,
+//             curveness: 0.2
+//           }
+//         },
+//         data: convertData(item[1])
+//       },
+//       {
+//         //name: item[0] + " Top3",
+//         name: "temp",
+//         type: "lines",
+//         zlevel: 2,
+//         symbol: ["none", "arrow"],
+//         symbolSize: 10,
+//         effect: {
+//           show: true,
+//           period: 6,
+//           trailLength: 0,
+//           symbol: planePath,
+//           symbolSize: 15
+//         },
+//         lineStyle: {
+//           normal: {
+//             color: color[i],
+//             width: 1,
+//             opacity: 0.6,
+//             curveness: 0.2
+//           }
+//         },
+//         data: convertData(item[1])
+//       },
+//       {
+//         name: "temp",
+//         //name: "Cities",
+//         type: "effectScatter",
+//         coordinateSystem: "geo",
+//         zlevel: 2,
+//         rippleEffect: {
+//           brushType: "stroke"
+//         },
+//         label: {
+//           normal: {
+//             show: true,
+//             position: "right",
+//             formatter: "{b}"
+//           }
+//         },
+//         symbolSize: function (val) {
+//           return val[2] / 8;
+//         },
+//         itemStyle: {
+//           normal: {
+//             color: color[i]
+//           },
+//           emphasis: {
+//             areaColor: "#2B91B7"
+//           }
+//         },
+//         data: item[1].map(function (dataItem) {
+//           return {
+//             name: dataItem[1].name,
+//             value: geoCoordMap[dataItem[1].name].concat([dataItem[1].value])
+//           };
+//         })
+//       }
+//     );
+//   });
+//   var option = {
+//     tooltip: {
+//       trigger: "item",
+//       formatter: function (params, ticket, callback) {
+//         if (params.seriesType == "effectScatter") {
+//           return "线路：" + params.data.name + "" + params.data.value[2];
+//         } else if (params.seriesType == "lines") {
+//           return (
+//             params.data.fromName +
+//             ">" +
+//             params.data.toName +
+//             "<br />" +
+//             params.data.value
+//           );
+//         } else {
+//           return params.name;
+//         }
+//       }
+//     },
+//     // legend: {
+//     //   orient: "vertical",
+//     //   top: "bottom",
+//     //   left: "right",
+//     //   data: ["西安 Top3", "西宁 Top3", "银川 Top3"],
+//     //   textStyle: {
+//     //     color: "#fff"
+//     //   },
+//     //   selectedMode: "multiple"
+//     // },
+//     geo: {
+//       map: "china",
+//       label: {
+//         emphasis: {
+//           show: true,
+//           color: "#fff"
+//         }
+//       },
+//       // 把中国地图放大了1.2倍
+//       zoom: 1.2,
+//       roam: true,
+//       itemStyle: {
+//         normal: {
+//           // 地图省份的背景颜色
+//           areaColor: "rgba(20, 41, 87,0.6)",
+//           borderColor: "#195BB9",
+//           borderWidth: 1
+//         },
+//         emphasis: {
+//           areaColor: "#2B91B7"
+//         }
+//       }
+//     },
+//     series: series
+//   };
+//   myChart.setOption(option);
+//   // 监听浏览器缩放，图表对象调用缩放resize函数
+//   window.addEventListener("resize", function () {
+//     myChart.resize();
+//   });
+// })();
